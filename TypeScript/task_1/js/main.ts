d98e9b8d96bf759d33c97b5ce4d509ebc8bd0cd8
sarahwcqz <sarah.wacquiez@gmail.com>
2026-01-18 11:51:00 +0100

    task 3 : function w/ interface

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+// ----------- TASK 1 ----------------
 interface Teacher {
     readonly firstName: string;
     readonly lastName: string;
@@ -6,7 +7,48 @@ interface Teacher {
     location: string;
     [key: string]: any;
 }
+// To help you correct bru
+const teacher1: Teacher = {
+  firstName: 'Marie',
+  lastName: 'Curie',
+  fullTimeEmployee: true,
+  location: 'Paris',
+  contract: true,
+};
+console.log("task 1");
+console.log(teacher1);
 
+
+
+// -------------- TASK 2 --------------
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+// To help you correct bru
+const director1: Directors = {
+  firstName: 'Indra',
+  lastName: 'Nooyi',
+  fullTimeEmployee: true,
+  location: 'New York',
+  numberOfReports: 120000,
+  contract: true,
+};
+console.log("task 2");
+console.log(director1);
+
+
+
+// ---------------- TASK 3 -------------
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+// To help you correct bru
+console.log("task 3");
+console.log(printTeacher('Marie', 'Curie'));
+
+// 
